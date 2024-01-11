f2b830c4598352099265d1d27ca1b01e20150cce
hllsebastian <hllsebastian@gmail.com>
2024-01-11 06:09:22 -0500

    Defining clean architecture

---
+++ tv_catalogue/lib/app/presentation/routes/app_routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../pages/splash/views/splash_view.dart';
+import 'routes.dart';
+
+Map<String, Widget Function(BuildContext)> get appRoutes {
+  return {
+    Routes.splash: (context) => const SplashView(),
+  };
+}
---
+++ tv_catalogue/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tv_catalogue/app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
